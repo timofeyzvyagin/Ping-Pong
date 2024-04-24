2f8fb99f692ed2fd4e67214f37887bce7b5a88e1
timofeyzvyagin <95702663+timofeyzvyagin@users.noreply.github.com>
2024-04-24 17:40:44 +0300

    Update main.py
    the end.

--- main.py
+++ main.py
@@ -56,6 +56,8 @@ game = True
 finish = False
 clock = time.Clock()
 FPS = 60
+speed_x = 5
+speed_y = 5
 
 racket1 = Player('racket.png', 30, 200, 50, 150, 4)
 racket2 = Player('racket.png', 520, 200, 50, 150, 4)
@@ -70,11 +72,31 @@ while game:
         window.fill(back)
         racket1.update_l()
         racket2.update_r()
+        ball.rect.x += speed_x
+        ball.rect.y += speed_y
+
+        if sprite.collide_rect(racket1, ball) or sprite.collide_rect(racket2, ball):
+            speed_x *= -1
+            
+
+        if ball.rect.y > win_height-50 or ball.rect.y < 0:
+            speed_y *= -1
+
+        if ball.rect.x < 0:
+            finish = True
+            window.blit(lose1, (200, 200))
+            game_over = True
+
+        if ball.rect.x > win_width:
+            finish = True
+            window.blit(lose2, (200, 200))
+            game_over = True         
 
         
 
     racket1.reset()
     racket2.reset()
+    ball.reset()
 
     display.update()
     clock.tick(FPS)        
